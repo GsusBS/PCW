16f1bc057c8fa9f76ee2f65864565cac788089c1
AnderIchaso <anderichaso@hotmail.com>
2021-06-01 13:24:53 +0200

    Preparacion Examen

---
+++ js/examen4R.js
@@ -0,0 +1,16 @@
+// Fichas 0: vacia
+// Fichas 1: Jugador 1
+// Fichas 2: Jugador 2
+
+var tablero = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+];
+
+// Jugador 1: True
+// Jugador 2: False
+var turno = true;
