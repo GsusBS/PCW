34dd26047ab6d383e92ec9488093e8231de305fd
AnderIchaso <anderichaso@hotmail.com>
2021-06-03 20:50:49 +0200

    Paginacion y Eventos

--- js/examenCalendar.js
+++ js/examenCalendar.js
@@ -1,4 +1,5 @@
 var hoy = new Date();
+var seleccion = new Date();
 var tablero = [
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
@@ -7,10 +8,19 @@ var tablero = [
     [0, 0, 0, 0, 0, 0, 0]
 ];
 
-class calendar{
-    constructor(){
+var eventillos = [
+    [new Date(2021, 05, 14), "Incendiar la Uni"],
+    [new Date(2021, 05, 27), "Pelear con patos"],
+    [new Date(2021, 05, 04), "Aprobar PCW"]
+
+
+];
+
+
+class calendar {
+    constructor() {
         this.tablero = tablero;
-        let cont=0;
+        let cont = 0;
         for (let i = 0; i < tablero.length; i++) {
             for (let j = 0; j < tablero[i].length; j++) {
                 tablero[i][j] = new Date(hoy.getFullYear(), hoy.getMonth(), cont);
@@ -18,10 +28,34 @@ class calendar{
             }
         }
     }
+
+    seleccionMesAno() {
+        let cont = 0;
+        for (let i = 0; i < tablero.length; i++) {
+            for (let j = 0; j < tablero[i].length; j++) {
+                tablero[i][j] = new Date(seleccion.getFullYear(), seleccion.getMonth(), cont);
+                cont++;
+            }
+        }
+
+    }
 }
+let calendario = new calendar();
 
 function empezar() {
 
+    //console.log(calendario)
+
+    imprimirCalendario(); //Muestra el calendario por pantalla (rellena el table)
+    sacarMesAno(); // Para la paginacion de arriba, saca el mes y año por pantalla
+    eventosProximos(); // Lista de eventos 
+
+
+
+
+}
+
+function imprimirCalendario() {
     let html = '';
 
     html += `<tr id="cabecera">`;
@@ -34,11 +68,109 @@ function empezar() {
     html += `<th>D</th>`;
     html += `</tr>`;
     document.getElementById("calendario").innerHTML = html;
+    for (let i = 0; i < calendario.tablero.length; i++) {
+        html += `<tr>`;
+        for (let j = 0; j < calendario.tablero[i].length; j++) {
+            if (calendario.tablero[i][j].getMonth() == hoy.getMonth()) {
 
-    console.log(hoy.getDay());
-    let calendario = new calendar();
-    console.log(calendario)
+                if (calendario.tablero[i][j].getDay() == 0) {
+                    html += `<th class="domingo">`;
+                } else {
+                    html += `<th>`;
+                }
 
+                html += calendario.tablero[i][j].getDate();
+            } else {
+                html += `<th>`;
+                html += ` `;
+            }
+            html += `</th>`;
+
+        }
+        html += `</tr>`;
+    }
+    document.getElementById("calendario").innerHTML = html;
+}
+
+function sacarMesAno() {
+    let mes = mesPalabra();
+    let html2 = "";
+    html2 += `<p>`;
+    html2 += mes;
+    html2 += `, `;
+    html2 += seleccion.getFullYear();
+    html2 += `</p>`;
+    document.getElementById("fecha").innerHTML = html2;
+}
+
+function mesPalabra() {
+    let mes = "Enero";
+    switch (seleccion.getMonth()) {
+        case 1:
+            mes = "Febrero";
+            break;
+        case 2:
+            mes = "Marzo";
+            break;
+        case 3:
+            mes = "Abril";
+            break;
+        case 4:
+            mes = "Mayo";
+            break;
+        case 5:
+            mes = "Junio";
+            break;
+        case 6:
+            mes = "Julio";
+            break;
+        case 7:
+            mes = "Agosto";
+            break;
+        case 8:
+            mes = "Septiembre";
+            break;
+        case 9:
+            mes = "Octubre";
+            break;
+        case 10:
+            mes = "Noviembre";
+            break;
+        case 11:
+            mes = "Diciembre";
+            break;
+
+
+    }
+
+
+    return mes;
+}
+
+function anterior() {
+    let nuevoMes = seleccion.getMonth();
+    seleccion.setMonth(nuevoMes - 1);
+    calendario.seleccionMesAno();
+    sacarMesAno();
+    imprimirCalendario();
+
+}
+
+function siguiente() {
+    let nuevoMes = seleccion.getMonth();
+    seleccion.setMonth(nuevoMes + 1);
+    calendario.seleccionMesAno();
+    sacarMesAno();
+    imprimirCalendario();
+
+}
+
+function eventosProximos() {
+    let html = '';
+    for (i = 0; i < eventillos.length; i++) {
+        html += `<li> {` + eventillos[i][0].getDate() + `-` + eventillos[i][0].getMonth() + '-' + eventillos[i][0].getFullYear() + `, ` + eventillos[i][0].getHours() + ':' + eventillos[i][0].getMinutes() + ` } ` + eventillos[i][1] + `</li>`;
+    }
+    document.getElementById("eventos").innerHTML = html;
 
 
 
